57fa3854c3f61ade43acf0f654d8234487b49dc0
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-08-03 12:21:26 -0400

    andre bio

--- src/app/lib/programs.ts
+++ src/app/lib/programs.ts
@@ -50,17 +50,35 @@ His mission as a broadcaster is to help create an awareness of any Guyanese pers
     title: "The Meltdown",
     time: "18:00 - 19:00",
     host: "Annmarie",
-    image: "https://i.pravatar.cc/200?img=6",
-    bio: "",
+    image: "/personalities/annmarie.jpg",
+    bio: `Step into the world of musical nostalgia with "The Meltdown" hosted by the enchanting Annmarie! This show takes you on a magical journey through time, embracing the charm of vintage rock, soca oldies, and timeless classics.
+
+From 18:00 to 19:00, every Monday, Tuesday, Thursday, and Friday, Annmarie graces the airwaves with her captivating voice and infectious enthusiasm. Her passion for music shines through as she curates a playlist that's sure to warm your heart and lift your spirits.
+
+"The Meltdown" is a delightful blend of feel-good vibes and soulful melodies, making it an absolute pleasure to listen to. Annmarie's dulcet tones, paired with the mesmerizing music, create an atmosphere that soothes the soul and transports you to a world of musical bliss.
+
+But it's not just her voice that captivates; Annmarie's radiant personality and wonderful soul shine through in every word she speaks. And let's not forget to mention her stunning beauty, which adds an extra layer of enchantment to the show.
+
+So, as the sun sets and the evening begins, tune in to "The Meltdown" and let Annmarie whisk you away to a place where the magic of vintage rock and timeless classics come alive. It's an hour of pure bliss and musical enchantment that you won't want to miss!`,
 
   },
   {
-    id: 'eveningvibe',
-    title: "Evening Vibe",
-    time: "19:00 - 20:00",
-    host: "Annmarie",
-    image: "https://i.pravatar.cc/200?img=14",
-    bio: "",
+    id: 'eveningvibez',
+    title: "Evening Vibez",
+    time: "19:00 - 21:00",
+    host: "Selector Andre",
+    image: "/personalities/selectorandre.jpg",
+    bio: `Bless up massive, it's the one and only Beenie Man, bringing you the irie vibes of "Evening Vibez" from 19:00 to 21:00, Monday to Friday! Ya know we got that dancehall and reggae all-round good music, straight from the heart of the Caribbean.
+
+Selector Andre, mi bredren, got that real Caribbean influence flowin' in the mix, and when mi seh flowin', mi mean it bubblin' like the sweetest Guyanese cookup. He's got the dancehall magic that can turn any yard into a yard a yard dance inna Jamaica, no doubt!
+
+We talkin' 'bout the authentic island vibes, where the music caresses your soul and the beat moves ya feet like it's carnival time. Selector Andre, him a boss DJ, him a real dancehall ambassador, and him know di ting!
+
+The station may be in Guyana, but mi waan everybody to feel the love and warmth of the Caribbean right through di airwaves. Selector Andre gonna take yuh on a musical journey like no other, where every tune tells a story, and every riddim carries the spirit of the islands.
+
+So, pull up a chair and turn up di volume, mi people! "Evening Vibez" with Selector Andre and Maad 97 FM inna di mix - a combination dat naah go wrong. From Guyana to di world, we keep the Caribbean flag flyin' high, and the dancehall fire blazing bright!
+
+Selecta, wheel it!♪♫ 'Cause the vibes, it's gonna nice up di evening, yeah!♪♫`,
 
   },
   {
